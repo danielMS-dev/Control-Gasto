8c87026a5dcbf9c8010814d1cdbe5603949fea0e
DMontanares <jdmontanares.santis@gmail.com>
2020-10-27 00:46:42 -0300

    Se crea ScheemaFormaPago

---
+++ models/formaPago.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const formaPagoScheema  = new mongoose.Schema(
+    {
+        nombre: {
+            type: String,
+            trim: true,
+            required: true,
+            maxlength: 100,
+            unique: true
+        },
+        descripcion: {
+            type: String,
+            trim: true,
+            maxlength: 500
+        }
+    },
+    { timestamps: true }
+);
+
+module.exports =mngoose.model("formPago", formaPagoScheema);
